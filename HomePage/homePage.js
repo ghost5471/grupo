4b2ff732e32539359e9c760cd7b68712e05ae288
tenmenezes <tenn.menezes@gmail.com>
2025-06-02 19:33:58 -0300

    configurando home-page

---
+++ HomePage/homePage.js
@@ -0,0 +1,8 @@
+ function verOferta() {
+      alert("Redirecionando para as ofertas!");
+    }
+
+    function toggleMenu() {
+      const menu = document.getElementById("floating-menu");
+      menu.classList.toggle("active");
+    }
