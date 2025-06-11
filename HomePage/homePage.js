bbd8ca29ab6192ee6249d7463b883a25255e4d9b
tenmenezes <tenn.menezes@gmail.com>
2025-06-11 15:32:48 -0300

    alterações na homepage

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -1,8 +1,5 @@
- function verOferta() {
-      alert("Redirecionando para as ofertas!");
-    }
-
-    function toggleMenu() {
+function toggleMenu() {
       const menu = document.getElementById("floating-menu");
       menu.classList.toggle("active");
-    }
+}
+
