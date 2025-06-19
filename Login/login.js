009dc9f552377560e8a0619b21db017bd2e4a724
Thiago Rocha <108189713+Thiago-Rock@users.noreply.github.com>
2025-06-19 09:56:45 -0300

    Renovação do meu branch com o que tinha feito offline

---
+++ Login/login.js
@@ -0,0 +1,41 @@
+// função para validação de login 
+document.getElementById('formLog').addEventListener('submit', function(event)
+{
+    event.preventDefault(); // impede o envio do formulário
+
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    const userName = document.getElementById('user-name').value;
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const validUser = users.find(user => user.email === email && user.senha === senha);
+
+    if(!validUser) {
+        redirecionarComErro("Erro: E-mail ou senha inválidos.");
+        return;
+    }
+
+    if(validUser) {
+        userName.textContent = JSON.stringify(email);
+    }
+
+    alert('Login bem-sucedido!');
+    localStorage.setItem('usuarioLogado', JSON.stringify(validUser));
+    window.location.href = '../index.html';
+
+});
+
+// Função para alternar a visibilidade da senha
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const button = input.nextElementSibling;
+    
+    if (input.type === 'password') {
+        input.type = 'text';
+        button.textContent = '🔓';
+    } else {
+        input.type = 'password';
+        button.textContent = '🔒';
+    }
+}
